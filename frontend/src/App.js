2b709e6b95fc61e901090cef0edf2c36ed4af25a
nollipistic-hue <nollipistic@gmail.com>
2026-09-15 16:38:54 +0530

    Add App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+// Pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
