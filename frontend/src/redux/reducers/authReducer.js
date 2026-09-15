632a55960fbdcf6e349a8775589872f74cf5946e
nollipistic-hue <nollipistic@gmail.com>
2026-09-15 16:45:27 +0530

    Add authReducer

---
+++ frontend/src/redux/reducers/authReducer.js
@@ -0,0 +1,35 @@
+const initialState = {
+  user: null,
+  token: localStorage.getItem('token'),
+  isAuthenticated: !!localStorage.getItem('token'),
+  loading: false,
+  error: null,
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'LOGIN_START':
+    case 'REGISTER_START':
+      return { ...state, loading: true, error: null };
+    case 'LOGIN_SUCCESS':
+    case 'REGISTER_SUCCESS':
+      localStorage.setItem('token', action.payload.token);
+      return {
+        ...state,
+        user: action.payload.user,
+        token: action.payload.token,
+        isAuthenticated: true,
+        loading: false,
+      };
+    case 'LOGIN_FAILURE':
+    case 'REGISTER_FAILURE':
+      return { ...state, loading: false, error: action.payload };
+    case 'LOGOUT':
+      localStorage.removeItem('token');
+      return { ...state, user: null, token: null, isAuthenticated: false };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
